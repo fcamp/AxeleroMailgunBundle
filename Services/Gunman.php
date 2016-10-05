3685b3bb4ebfda11a785748c188dc54ea9ee969b
maurodalatri <mauro@dalatri.it>
2016-10-05 10:45:41 +0200

    Add more recipients data on batchSend

--- Services/Gunman.php
+++ Services/Gunman.php
@@ -85,11 +85,11 @@ class Gunman
         $bb->setTextBody($this->cleanUp($body));
 
         $notCustom = ['email', 'first', 'last'];
-        foreach ($recipients as $r) {
-            $bb->addToRecipient($r['email'], array_filter(['first' => $r['first'], 'last' => $r['last']]));
+        foreach ($recipients as $recipient) {
+            $bb->addToRecipient($recipient['email'], $recipient);
 
             //all the other fields are considered as custom data
-            foreach ($this->array_filter_key($r, function ($i) use ($notCustom) {
+            foreach ($this->array_filter_key($recipient, function ($i) use ($notCustom) {
                 return array_search($i, $notCustom) === false;
             }) as $k => $v) {//
                 $bb->addCustomData($k, $v);
